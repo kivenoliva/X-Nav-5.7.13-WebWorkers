5915d15ddf206dffeb7f57324c9447bae7ccb187
kivenoliva <kivenoliva@gmail.com>
2015-03-23 14:20:06 +0100

    problema acabado

---
+++ webworkers.js
@@ -0,0 +1,16 @@
+self.onmessage = function(event) {
+
+    var n = 1;
+    primelist = "";
+    search: while (n<parseInt(event.data)) {
+        n += 1;
+        for (var i = 2; i <= Math.sqrt(n); i += 1)
+          if (n % i == 0)
+            continue search;
+        // found a prime!
+        primelist += " " + n;
+    }
+    self.postMessage(primelist);
+}
+
+
